32ae1fdc4c397e5d3236af761e252a65c9f77bae
Abass14 <abasstoy14@gmail.com>
2022-07-06 15:03:00 -0400

    added sample component

---
+++ src/components/Sample/Sample.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Sample = () : JSX.Element => {
+    return (
+        <View>
+            <Text>Sample</Text>
+        </View>
+    )
+}
+
+export default Sample;
